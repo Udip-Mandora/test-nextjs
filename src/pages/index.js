5e54f4609392ef250ac9826174837b25fec67969
Udip <udipmandora42@gmail.com>
2023-04-24 01:39:34 -0400

    Added image

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,6 +27,13 @@ export default function Home() {
         <h3>Welcome to My Portfolio</h3>
         <p>Here, we will see the work I have done over the time.</p>
         <p>Each link will take you on a tour to the designated page and show you the exact work which has been done by me.</p>
+
+        <p>I have done my post graduation in Web Development from Humber College. I have been an enthusiast whenever it came to technology
+          and building websites. My goal always has been to work and develop websites and products which can change people's lives with
+          that.
+        </p>
+
+        <img id="desk" src="./image/desk.jpg"></img>
       </main>
 
       <footer class="footer">
